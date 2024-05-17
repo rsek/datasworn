93d33ff2be59677528ebf025a9e73f32fea26688
rsek <5354757+rsek@users.noreply.github.com>
2024-05-16 18:26:39 -0700

    sketch for later use

---
+++ src/scripts/codegen/schemaToTsClass.ts
@@ -0,0 +1,15 @@
+// TODO
+
+// type ClassPropData = {
+// 	id: string
+// 	type: string
+// 	readonly: boolean
+// 	default: string
+// }
+
+// type ClassData = {
+// 	id: string
+// 	extends?: string
+// 	implements?: string
+// 	properties: ClassPropData
+// }
