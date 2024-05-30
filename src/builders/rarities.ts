508506ceca1baaeb6203877c7e6465fec7d70f84
rsek <5354757+rsek@users.noreply.github.com>
2024-05-30 16:31:15 -0700

    rm old builders

--- src/builders/rarities.ts
+++
@@ -1,9 +0,0 @@
-import { sourcedTransformer } from './transformer.js'
-import type * as Datasworn from '../types/Datasworn.js'
-import type * as DataswornSource from '../types/DataswornSource.js'
-
-export const Rarity = sourcedTransformer<
-	DataswornSource.Rarity,
-	Datasworn.Rarity,
-	null
->({})
--- src/scripts/datasworn/buildRulesetData.ts
+++ src/scripts/datasworn/buildRulesetData.ts
@@ -1,27 +1,10 @@
-import { RulesPackage as DataswornBuilder } from '../../builders/datasworn.js'
-import { transform } from '../../builders/transformer.js'
-import type * as Generic from '../../schema/Generic.js'
-import type * as DataswornSource from '../../types/DataswornSource.js'
-import type * as Datasworn from '../../types/Datasworn.js'
 import { assignIds } from '../../schema/generateId.js'
+import type * as DataswornSource from '../../types/DataswornSource.js'
 
 /** Builds from the contents of a single YAML or JSON file */
 export async function buildRulesetData(
 	sourceData: DataswornSource.RulesPackage
 ) {
-	// const builtData = transform<
-	// 	DataswornSource.RulesPackage,
-	// 	Datasworn.RulesPackage,
-	// 	unknown
-	// >(
-	// 	sourceData,
-	// 	sourceData._id,
-	// 	sourceData as DataswornSource.RulesPackage & Generic.SourcedNode,
-	// 	DataswornBuilder as any
-	// )
-
-	// return builtData
-
 	assignIds(sourceData)
 
 	return sourceData
