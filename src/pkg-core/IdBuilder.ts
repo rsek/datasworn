034ee136eab2afbb0dd12c6b2883541ad8e472a6
rsek <5354757+rsek@users.noreply.github.com>
2024-06-07 12:52:24 -0700

    add some documentation, renames

--- src/pkg-core/IdBuilder.ts
+++ src/pkg-core/IdBuilder.ts
@@ -1,5 +1,6 @@
 import type DataswornNode from './DataswornNode.js'
 import CONST from './IdElements/CONST.js'
+import type NodeTypeId from './IdElements/NodeTypeId.js'
 import Pattern from './IdElements/Pattern.js'
 import type { Datasworn } from './index.js'
 
@@ -28,9 +29,10 @@ type EntryInProp<O, P extends DictPropKeyIn<O> | ArrayPropKeyIn<O>> =
 type WalkIteratee<T> = (value: T, pathKeys: string[]) => any
 
 /**
- * @template From The object type of the starting point of this path element.
+ * Represents a single, slash-separated path element of a Datasworn ID.
+ * @template Origin The object type of the starting point of this path element.
  */
-abstract class PathSymbol<From, Key extends keyof From = keyof From> {
+abstract class PathSymbol<Origin, Key extends keyof Origin = keyof Origin> {
 	readonly inProperty: Key
 	abstract get pattern(): RegExp
 
@@ -41,170 +43,191 @@ abstract class PathSymbol<From, Key extends keyof From = keyof From> {
 	}
 }
 
-class RulesPackageSymbol extends PathSymbol<null> {
-	get pattern() {
-		return Pattern.RulesPackageElement
-	}
-
-	constructor() {
-		// @ts-expect-error
-		super(null)
-	}
-
-	clone() {
-		return new RulesPackageSymbol() as this
-	}
-}
-
-class DictKey<Origin, Prop extends DictPropKeyIn<Origin>> extends PathSymbol<
-	Origin,
-	Prop
-> {
-	public get minReps(): number {
-		return CONST.RECURSIVE_PATH_ELEMENTS_MIN
-	}
-
-	public get maxReps(): number {
-		return CONST.RECURSIVE_PATH_ELEMENTS_MIN
-	}
-
-	get pattern() {
-		return Pattern.DictKeyElement
-	}
-
-	clone() {
-		return new DictKey<Origin, Prop>(this.inProperty) as this
-	}
-
-	static get stringTemplateLiteral() {
-		// eslint-disable-next-line no-template-curly-in-string
-		return '${string}'
-	}
-
-	static repsStringTemplateLiteral(min: number, max: number) {
-		let variantsToGenerate = max - min + 1
-
-		const variants: string[] = []
-
-		while (variantsToGenerate > 0) {
-			const variantParts: string[] = []
-
-			for (let i = 0; i < variantsToGenerate; i++)
-				variantParts.push(DictKey.stringTemplateLiteral)
-
-			variants.push(variantParts.join(CONST.PathSep))
-
-			variantsToGenerate--
-		}
-		return variants
-	}
-
-	static repsRegex(min: number, max?: number): RegExp {
-		let minMax: string
-
-		switch (true) {
-			case min === 1 && max === 1:
-				return Pattern.DictKeyElement
-			case typeof max !== 'number':
-			case max === min:
-				minMax = `{${min}}`
-				break
-			default:
-				minMax = `{${min},${max}}`
-				break
+namespace PathSymbol {
+	export class RulesPackage extends PathSymbol<null> {
+		get pattern() {
+			return Pattern.RulesPackageElement
 		}
 
-		const src = `(?:${IdPattern.PathSep}${Pattern.DictKeyElement.source})${minMax}`
+		constructor() {
+			// @ts-expect-error
+			super(null)
+		}
 
-		return new RegExp(src)
+		clone() {
+			return new PathSymbol.RulesPackage() as this
+		}
 	}
 
-	static reduceReps(...dictKeys: DictKey<any, any>[]) {
-		const min = dictKeys.reduce((a, b) => a + b.minReps, 0)
-		const max = dictKeys.reduce((a, b) => a + b.maxReps, 0)
-		return { min, max }
+	export class DictKey<
+		Origin,
+		Prop extends DictPropKeyIn<Origin>
+	> extends PathSymbol<Origin, Prop> {
+		public get minReps(): number {
+			return CONST.RECURSIVE_PATH_ELEMENTS_MIN
+		}
+
+		public get maxReps(): number {
+			return CONST.RECURSIVE_PATH_ELEMENTS_MIN
+		}
+
+		get pattern() {
+			return Pattern.DictKeyElement
+		}
+
+		clone() {
+			return new DictKey<Origin, Prop>(this.inProperty) as this
+		}
+
+		static get stringTemplateLiteral() {
+			// eslint-disable-next-line no-template-curly-in-string
+			return '${string}'
+		}
+
+		static repsStringTemplateLiteral(min: number, max: number) {
+			let variantsToGenerate = max - min + 1
+
+			const variants: string[] = []
+
+			while (variantsToGenerate > 0) {
+				const variantParts: string[] = []
+
+				for (let i = 0; i < variantsToGenerate; i++)
+					variantParts.push(DictKey.stringTemplateLiteral)
+
+				variants.push(variantParts.join(CONST.PathSep))
+
+				variantsToGenerate--
+			}
+			return variants
+		}
+
+		static repsRegex(min: number, max?: number): RegExp {
+			let minMax: string
+
+			switch (true) {
+				case min === 1 && max === 1:
+					return Pattern.DictKeyElement
+				case typeof max !== 'number':
+				case max === min:
+					minMax = `{${min}}`
+					break
+				default:
+					minMax = `{${min},${max}}`
+					break
+			}
+
+			const src = `(?:${IdPattern.PathSep}${Pattern.DictKeyElement.source})${minMax}`
+
+			return new RegExp(src)
+		}
+
+		static reduceReps(...dictKeys: DictKey<any, any>[]) {
+			const min = dictKeys.reduce((a, b) => a + b.minReps, 0)
+			const max = dictKeys.reduce((a, b) => a + b.maxReps, 0)
+			return { min, max }
+		}
+	}
+
+	export class RecursiveDictKeys<
+		Origin,
+		Prop extends DictPropKeyIn<Origin>,
+		RecursiveProp extends DictPropKeyIn<EntryInProp<Origin, Prop>>
+	> extends DictKey<Origin, Prop> {
+		readonly recursiveProperty: RecursiveProp
+
+		override get pattern() {
+			return Pattern.RecursiveDictKeysElement
+		}
+
+		override get minReps() {
+			return CONST.RECURSIVE_PATH_ELEMENTS_MIN
+		}
+
+		override get maxReps() {
+			return CONST.RECURSIVE_PATH_ELEMENTS_MAX
+		}
+
+		constructor(inProperty: Prop, recursiveProperty: RecursiveProp) {
+			super(inProperty)
+			this.recursiveProperty = recursiveProperty
+		}
+
+		clone<
+			CloneProp extends DictPropKeyIn<Origin> = Prop,
+			CloneRecursiveProp extends DictPropKeyIn<
+				EntryInProp<Origin, Prop>
+			> = RecursiveProp
+		>() {
+			// @ts-expect-error
+			return new RecursiveDictKeys<Origin, CloneProp, CloneRecursiveProp>(
+				this.inProperty,
+				this.recursiveProperty
+			) as this
+		}
+	}
+
+	export class Index<
+		Origin,
+		Key extends ArrayPropKeyIn<Origin>
+	> extends PathSymbol<Origin, Key> {
+		clone() {
+			return new Index<Origin, Key>(this.inProperty) as this
+		}
+
+		override get pattern() {
+			return Pattern.IndexElement
+		}
 	}
 }
 
-class RecursiveDictKeys<
-	Origin,
-	Prop extends DictPropKeyIn<Origin>,
-	RecursiveProp extends DictPropKeyIn<EntryInProp<Origin, Prop>>
-> extends DictKey<Origin, Prop> {
-	readonly recursiveProperty: RecursiveProp
-
-	override get pattern() {
-		return Pattern.RecursiveDictKeysElement
-	}
-
-	override get minReps() {
-		return CONST.RECURSIVE_PATH_ELEMENTS_MIN
-	}
-
-	override get maxReps() {
-		return CONST.RECURSIVE_PATH_ELEMENTS_MAX
-	}
-
-	constructor(inProperty: Prop, recursiveProperty: RecursiveProp) {
-		super(inProperty)
-		this.recursiveProperty = recursiveProperty
-	}
-
-	clone<
-		CloneProp extends DictPropKeyIn<Origin> = Prop,
-		CloneRecursiveProp extends DictPropKeyIn<
-			EntryInProp<Origin, Prop>
-		> = RecursiveProp
-	>() {
-		// @ts-expect-error
-		return new RecursiveDictKeys<Origin, CloneProp, CloneRecursiveProp>(
-			this.inProperty,
-			this.recursiveProperty
-		) as this
-	}
-}
-
-class Index<Origin, Key extends ArrayPropKeyIn<Origin>> extends PathSymbol<
-	Origin,
-	Key
-> {
-	clone() {
-		return new Index<Origin, Key>(this.inProperty) as this
-	}
-
-	override get pattern() {
-		return Pattern.IndexElement
-	}
-}
-
-class IdPattern<Origin = Datasworn.RulesPackage> extends Array<TypePathFormat> {
+/**
+ * Represents a complete Datasworn ID, consisting of one or more type prefixes
+ * @template CurrentNode The type of the current (last) node in an IdPattern. Used to provide typechecks for keys when chaining `add` methods.
+ */
+class IdPattern<
+	CurrentNode = Datasworn.RulesPackage
+> extends Array<PathFormat> {
 	static readonly PathTypeSep = '\\' + CONST.PathTypeSep
 
 	static readonly PrefixSep = CONST.PrefixSep
 
 	static readonly PathSep = '\\' + CONST.PathSep
 
-	static assignIds<T extends Datasworn.RulesPackage>(
-		root: T,
-		pattern: IdPattern<T>
-	) {}
-
-	getLeftSide() {
-		return this.map(({ typeId }) => typeId).join(IdPattern.PathTypeSep)
+	/**
+	 * Get the left side of the ID pattern: the type prefix.
+	 * @example `oracle_rollable`
+	 * @example `move_category`
+	 * @example `asset.ability.move`
+	 */
+	getLeftSide(): RegExp {
+		return new RegExp(
+			this.map(({ typeId }) => typeId).join(IdPattern.PathTypeSep)
+		)
 	}
 
-	getRightSide(groups: RegexGroupType = 'named_capture_group') {
+	/**
+	 * Get the right hand side of the ID pattern: the path.
+	 * @example `starforged/core/action`
+	 * @example `classic/suffer`
+	 * @example `classic/ritual/commune.0.commune`
+	 */
+	getRightSide(groups: RegexGroupType = 'named_capture_group'): RegExp {
 		const result = this.map((part) => part.toRegexSource(groups)).join(
 			IdPattern.PathTypeSep
 		)
 
-		return result
+		return new RegExp(result)
 	}
 
+	/**
+	 * Return a new copy of this IdPattern.
+	 * @param primaryTypeId An optional override for the typeId of the *last* PathFormat.
+	 */
 	clone(primaryTypeId?: string) {
 		return new IdPattern(
-			...this.map((el, i) =>
-				typeof primaryTypeId === 'string' && i === 0
+			...this.map((el, i, arr) =>
+				typeof primaryTypeId === 'string' && i === arr.length - 1
 					? el.clone(primaryTypeId)
 					: el.clone()
 			)
@@ -216,31 +239,33 @@ class IdPattern<Origin = Datasworn.RulesPackage> extends Array<TypePathFormat> {
 	}
 
 	get current() {
-		return (this.at(-1) as TypePathFormat<Origin>) ?? null
+		return (this.at(-1) as PathFormat<CurrentNode>) ?? null
 	}
 
 	addNewTypeGroup(typeId: string) {
-		this.push(new TypePathFormat(typeId))
+		this.push(new PathFormat(typeId))
 		return this
 	}
 
-	addDictKey<Key extends DictPropKeyIn<Origin>>(inProperty: Key) {
+	addDictKey<Key extends DictPropKeyIn<CurrentNode>>(inProperty: Key) {
 		this.current.addDictKey(inProperty)
-		return this as IdPattern<Origin[Key][keyof Origin[Key]]>
+		return this as IdPattern<CurrentNode[Key][keyof CurrentNode[Key]]>
 	}
 
 	addRecursiveDictKeys<
-		Prop extends DictPropKeyIn<Origin>,
-		RecursiveProp extends DictPropKeyIn<Origin[Prop][keyof Origin[Prop]]>
+		Prop extends DictPropKeyIn<CurrentNode>,
+		RecursiveProp extends DictPropKeyIn<
+			CurrentNode[Prop][keyof CurrentNode[Prop]]
+		>
 	>(property: Prop, recursiveProperty: RecursiveProp) {
 		this.current.addRecursiveDictKeys(property, recursiveProperty)
-		return this as IdPattern<EntryInProp<Origin, Prop>>
+		return this as IdPattern<EntryInProp<CurrentNode, Prop>>
 	}
 
-	addIndex<Prop extends ArrayPropKeyIn<Origin>>(inProperty: Prop) {
+	addIndex<Prop extends ArrayPropKeyIn<CurrentNode>>(inProperty: Prop) {
 		this.current.addIndex<Prop>(inProperty)
 
-		return this as IdPattern<EntryInProp<Origin, Prop>>
+		return this as IdPattern<EntryInProp<CurrentNode, Prop>>
 	}
 
 	toRegex(
@@ -248,28 +273,85 @@ class IdPattern<Origin = Datasworn.RulesPackage> extends Array<TypePathFormat> {
 		groups: RegexGroupType = 'named_capture_group'
 	) {
 		const base =
-			this.getLeftSide() + IdPattern.PrefixSep + this.getRightSide(groups)
+			this.getLeftSide().source +
+			IdPattern.PrefixSep +
+			this.getRightSide(groups).source
 		if (lineDelimiters) return new RegExp('^' + base + '$')
 		return new RegExp(base)
 	}
 
+	toSchema() {}
+
 	static fromRoot(typeId: string) {
-		const f = new TypePathFormat(typeId, true)
+		const f = new PathFormat(typeId, true)
 		return new IdPattern(f)
 	}
 
-	static relativeTo() {}
-
-	constructor(...formats: TypePathFormat[]) {
+	constructor(...formats: PathFormat[]) {
 		super()
 		this.push(...formats)
+	}
+
+	static createNonCollectable<T extends NodeTypeId.NonCollectable>(
+		typeId: T,
+		typeRoot: NodeTypeId.RootKey<T>
+	) {
+		return IdPattern.fromRoot(typeId).addDictKey(typeRoot) as IdPattern<
+			DataswornNode.ByType<T>
+		>
+	}
+
+	static createRecursiveCollection<T extends NodeTypeId.Collection.Recursive>(
+		typeId: T,
+		typeRoot: NodeTypeId.RootKey<T>
+	) {
+		return (
+			IdPattern.fromRoot(typeId)
+				// @ts-expect-error this happens because not all union members of OracleCollection have the 'collections' property
+				.addRecursiveDictKeys(typeRoot, 'collections') as IdPattern<
+				DataswornNode.ByType<T>
+			>
+		)
+	}
+
+	static createRecursiveCollectable<T extends NodeTypeId.Collectable.Recursive>(
+		typeId: T,
+		typeRoot: NodeTypeId.RootKey<T>
+	) {
+		return (
+			IdPattern.fromRoot(typeId)
+				// @ts-expect-error
+				.addRecursiveDictKeys(typeRoot, 'collections')
+				// @ts-expect-error
+				.addDictKey('contents') as IdPattern<DataswornNode.ByType<T>>
+		)
+	}
+
+	static createNonRecursiveCollection<
+		T extends NodeTypeId.Collection.NonRecursive
+	>(typeId: T, typeRoot: NodeTypeId.RootKey<T>) {
+		return IdPattern.fromRoot(typeId).addDictKey(typeRoot) as IdPattern<
+			DataswornNode.ByType<T>
+		>
+	}
+
+	static createNonRecursiveCollectable<
+		T extends NodeTypeId.Collectable.NonRecursive
+	>(typeId: T, typeRoot: NodeTypeId.RootKey<T>) {
+		return (
+			IdPattern.fromRoot(typeId)
+				.addDictKey(typeRoot)
+				// @ts-expect-error not totally sure why this one gripes tho
+				.addDictKey('contents') as IdPattern<DataswornNode.ByType<T>>
+		)
 	}
 }
 
 /**
+ * Represents a slash-separated path in a Datasworn ID.
  * @template Origin The object type of the origin for this path.
  */
-class TypePathFormat<Origin = Datasworn.RulesPackage> extends Array<
+class PathFormat<Origin = Datasworn.RulesPackage> extends Array<
 	PathSymbol<any>
 > {
 	typeId: string
@@ -280,26 +362,28 @@ class TypePathFormat<Origin = Datasworn.RulesPackage> extends Array<
 
 	toRegexSource(group?: RegexGroupType) {
 		let base = ''
-		let dictKeys: DictKey<any, any>[] = []
+		let dictKeys: PathSymbol.DictKey<any, any>[] = []
 
 		for (let i = 0; i < this.length; i++) {
 			const part = this[i]
 			const nextPart = this[i + 1]
 
 			switch (true) {
-				case part instanceof RulesPackageSymbol:
+				case part instanceof PathSymbol.RulesPackage:
 					// no separator needed -- it's at the start
 					base += part.pattern.source
 					break
-				case part instanceof DictKey && nextPart instanceof DictKey:
+				case part instanceof PathSymbol.DictKey &&
+					nextPart instanceof PathSymbol.DictKey:
 					// DictKey chain not done; grab the current one and continue
 					dictKeys.push(part)
 					break
-				case part instanceof DictKey && !(nextPart instanceof DictKey): {
+				case part instanceof PathSymbol.DictKey &&
+					!(nextPart instanceof PathSymbol.DictKey): {
 					// end of this DictKey chain; assemble complete DictKey string, and reset array
 					dictKeys.push(part)
-					const { min, max } = DictKey.reduceReps(...dictKeys)
-					const toAppend = DictKey.repsRegex(min, max).source
+					const { min, max } = PathSymbol.DictKey.reduceReps(...dictKeys)
+					const toAppend = PathSymbol.DictKey.repsRegex(min, max).source
 					if (!toAppend.startsWith('(?:\\/')) base += IdPattern.PathSep
 					base += toAppend
 					dictKeys = []
@@ -330,27 +414,27 @@ class TypePathFormat<Origin = Datasworn.RulesPackage> extends Array<
 
 	constructor(typeId: string, relative = false) {
 		super()
-		if (relative) this.push(new RulesPackageSymbol())
+		if (relative) this.push(new PathSymbol.RulesPackage())
 		this.relative = relative
 		this.typeId = typeId
 	}
 
 	clone(newTypeId?: string) {
-		const clone = new TypePathFormat(newTypeId ?? this.typeId, this.relative)
+		const clone = new PathFormat(newTypeId ?? this.typeId, this.relative)
 		for (const part of this) {
-			if (part instanceof RulesPackageSymbol) continue
+			if (part instanceof PathSymbol.RulesPackage) continue
 			clone.push(part.clone())
 		}
 		return clone as this
 	}
 
 	createRelativeAs(newTypeId: string) {
-		return new TypePathFormat(newTypeId, true) as typeof this
+		return new PathFormat(newTypeId, true) as typeof this
 	}
 
 	addDictKey<Prop extends DictPropKeyIn<Origin>>(inProperty: Prop) {
-		this.push(new DictKey<Origin, Prop>(inProperty))
-		return this as TypePathFormat<Origin[Prop][keyof Origin[Prop]]>
+		this.push(new PathSymbol.DictKey<Origin, Prop>(inProperty))
+		return this as PathFormat<Origin[Prop][keyof Origin[Prop]]>
 	}
 
 	addRecursiveDictKeys<
@@ -358,17 +442,17 @@ class TypePathFormat<Origin = Datasworn.RulesPackage> extends Array<
 		RecursiveProp extends DictPropKeyIn<EntryInProp<Origin, Prop>>
 	>(property: Prop, recursiveProperty: RecursiveProp) {
 		this.push(
-			new RecursiveDictKeys<Origin, Prop, RecursiveProp>(
+			new PathSymbol.RecursiveDictKeys<Origin, Prop, RecursiveProp>(
 				property,
 				recursiveProperty
 			)
 		)
-		return this as TypePathFormat<EntryInProp<Origin, Prop>>
+		return this as PathFormat<EntryInProp<Origin, Prop>>
 	}
 
 	addIndex<Prop extends ArrayPropKeyIn<Origin>>(inProperty: Prop) {
-		this.push(new Index<Origin, Prop>(inProperty))
-		return this as TypePathFormat<EntryInProp<Origin, Prop>>
+		this.push(new PathSymbol.Index<Origin, Prop>(inProperty))
+		return this as PathFormat<EntryInProp<Origin, Prop>>
 	}
 }
 
@@ -407,4 +491,4 @@ console.log(assetAbility.toRegex())
 console.log(assetAbilityMove.toRegex())
 console.log(oracleCollection.toRegex())
 console.log(oracleRollable.toRegex())
-console.log(DictKey.repsStringTemplateLiteral(1, 3))
+console.log(PathSymbol.DictKey.repsStringTemplateLiteral(1, 3))
